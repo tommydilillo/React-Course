5b9f93a02f0986eb13ed6dfd648982774a8e0050
Tommy DiLillo <tommydilillo@gmail.com>
2018-11-13 23:30:33 -0500

    create base components: app, header, inventory, order

---
+++ src/components/Inventory.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+    render() {
+        return (
+
+            <div class="inventory">
+                Inventory!
+            </div>
+
+
+        )
+    }
+
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div class="order">
+                Order!
+        </div>
+
+
+        )
+    }
+
+}
+
+export default Order;
