18692443d8a816129005978269708f8d1bea8d50
Tommy DiLillo <tommydilillo@gmail.com>
2018-12-05 22:46:32 -0500

    persisted order state into local storage

--- src/components/App.js
+++ src/components/App.js
@@ -13,15 +13,30 @@ class App extends React.Component {
     };
     componentDidMount() {
         const { params } = this.props.match
+        // first reinstate our localStorage
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if (localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef) })
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: 'fishes'
         });
     }
 
+    componentDidUpdate() {
+        console.log(this.state.order);
+        localStorage.setItem
+            (this.props.match.params.storeId,
+            JSON.stringify(this.state.order)
+            );
+    }
+
+
     componentWillUnmount() {
         base.removeBinding(this.ref);
     }
+
 
     addFish = fish => {
         // 1. Take a copy of the existing state (don't want to mutate object)
